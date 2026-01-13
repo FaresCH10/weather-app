f35e7171ca67de342ef863d52ec2ca964a77c2d9
Fares Chrayteh <fareschrayteh1@gmail.com>
2026-01-13 10:59:51 +0200

    Weather API: HTML/CSS

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Raleway } from "next/font/google";
+import "./globals.css"
+
+const raleway = Raleway({
+  subsets: ["latin"],
+  variable: "--font-raleway"
+})
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,7 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className={`${geistSans.variable} ${geistMono.variable} ${raleway.variable}`}>
         {children}
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,22 @@ import React from 'react'
 
 const WeatherApp = () => {
   return (
-    <div>Fares Chrayteh</div>
+    <div className='container'>
+      <div className='search-container'>
+        <input type="text" className='search-bar' placeholder='Search City...' />
+      </div>
+      <div className='weather-container'>
+        <div>
+          <h2>London</h2>
+          <p>clear</p>
+          <h2 style={{fontSize: "3rem"}}>16°C</h2>
+        </div>
+        <img src="https://placekittens.com/g/200/300" alt="weather icons" width={100} height={100} />
+      </div>
+      <div className='footer-container'>
+        <p>Created by FaresCH</p>
+      </div>
+    </div>    
   )
 }
 
